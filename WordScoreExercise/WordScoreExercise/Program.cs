33dc79fb385a78aff4375674bec684fbe5c6b4f4
rsk2 <beingrohan24.7@gmail.com>
2020-04-14 13:48:53 +0530

    Update Program.cs
    Removing comment describing the app

--- WordScoreExercise/WordScoreExercise/Program.cs
+++ WordScoreExercise/WordScoreExercise/Program.cs
@@ -3,14 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-/*
-      Dollar Words:
-The input file american-words.txt is attached.
 
-Using a C# console application, give each letter in each word a numerical value equal to its position \
-in the alphabet.  So "a" is 1 and "z" is 26.  Punctuation and whitespace is 0.  
-Find words where the sum of the value of the letters is 100 and print each of the “100” words on the console. 
-     */
 
 namespace WordScoreExercise
 {
